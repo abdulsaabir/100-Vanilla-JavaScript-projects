b8a26f30c0b214c7d35c2b47e403efa4835c98b9
eng-abdulsaabirhaaji <saabirhaaji10@gmail.com>
2022-09-05 08:13:31 -0700

    Reviews-project

---
+++ Reviews-Project/js.js
@@ -0,0 +1,87 @@
+// local reviews data
+const reviews = [
+    {
+      id: 1,
+      name: "Susan Smith",
+      job: "Web Developer",
+      img:
+        "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg",
+      text:
+        "I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry",
+    },
+    {
+      id: 2,
+      name: "Anna Johnson",
+      job: "Web Designer",
+      img:
+        "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883409/person-2_np9x5l.jpg",
+      text:
+        "Helvetica artisan kinfolk thundercats lumbersexual blue bottle. Disrupt glossier gastropub deep v vice franzen hell of brooklyn twee enamel pin fashion axe.photo booth jean shorts artisan narwhal.",
+    },
+    {
+      id: 3,
+      name: "Peter Jones",
+      job: "Intern",
+      img:
+        "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg",
+      text:
+        "Sriracha literally flexitarian irony, vape marfa unicorn. Glossier tattooed 8-bit, fixie waistcoat offal activated charcoal slow-carb marfa hell of pabst raclette post-ironic jianbing swag.",
+    },
+    {
+      id: 4,
+      name: "Bill Anderson",
+      job: "The Boss",
+      img:
+        "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg",
+      text:
+        "Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. ",
+    },
+  ];
+
+  let personImage = document.getElementById("person-img");
+  let personName =   document.getElementById("person")
+  let personTitle = document.getElementById("title")
+  let personDescription = document.getElementById("desc")
+
+  let btnRight = document.getElementById("right")
+
+  let btnLeft = document.getElementById("left")
+  let counter = 0;
+
+  window.addEventListener("DOMContentLoaded" , function(){
+    PersonNow()
+  })
+
+  btnRight.addEventListener('click' , function(){
+    counter++
+    if (counter > reviews.length -1)
+    {
+        counter = 0
+    }
+    PersonNow()
+  })
+
+  btnLeft.addEventListener('click' , function(){
+    counter--
+    if(counter<0)
+    {
+        counter= reviews.length -1
+    }
+    PersonNow()
+  })
+
+  
+  let randomPerson = document.getElementById("suprise")
+
+  randomPerson.addEventListener('click' , function(){
+    counter = Math.floor(Math.random()* reviews.length)
+   PersonNow()
+
+  })
+
+  function PersonNow(){
+    personImage.src = reviews[counter].img
+    personName.textContent= reviews[counter].name
+    personTitle.textContent= reviews[counter].job
+    personDescription.textContent= reviews[counter].text
+  }
