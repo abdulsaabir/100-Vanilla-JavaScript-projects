a36289d4d5efafe5342b86557bde176681052c5a
eng-abdulsaabirhaaji <saabirhaaji10@gmail.com>
2022-09-06 09:18:55 +0300

    modal

--- Model-Project/app.js
+++ Model-Project/app.js
@@ -1,7 +1,8 @@
+let modal = document.querySelector('.two')
 let body = document.querySelector('body')
 let openModel = document.querySelector('.modal-btn')
 let closeModel = document.querySelector('.cancel')
-let modal = document.querySelector('.two')
+
 
 openModel.addEventListener('click' , function(){
     modal.classList.add('toggle')
