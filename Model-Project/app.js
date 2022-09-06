6cf9c376c941204271846e19f9ebae4dd31ff0b9
eng-abdulsaabirhaaji <saabirhaaji10@gmail.com>
2022-09-06 09:19:10 +0300

    modal

--- Model-Project/app.js
+++ Model-Project/app.js
@@ -1,7 +1,8 @@
+let closeModel = document.querySelector('.cancel')
 let modal = document.querySelector('.two')
 let body = document.querySelector('body')
 let openModel = document.querySelector('.modal-btn')
-let closeModel = document.querySelector('.cancel')
+
 
 
 openModel.addEventListener('click' , function(){
