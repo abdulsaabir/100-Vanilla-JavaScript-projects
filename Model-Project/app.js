59bb68e51bd8b3f0387eb62022da982e98400bdf
eng-abdulsaabirhaaji <saabirhaaji10@gmail.com>
2022-09-06 09:19:36 +0300

    modal

--- Model-Project/app.js
+++ Model-Project/app.js
@@ -1,7 +1,8 @@
+let openModel = document.querySelector('.modal-btn')
 let closeModel = document.querySelector('.cancel')
 let modal = document.querySelector('.two')
 let body = document.querySelector('body')
-let openModel = document.querySelector('.modal-btn')
+
 
 
 
