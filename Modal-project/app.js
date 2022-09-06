0125ccef7c111c684a826524b7f45fbb19193c17
eng-abdulsaabirhaaji <saabirhaaji10@gmail.com>
2022-09-05 21:19:28 -0700

    modal

---
+++ Modal-project/app.js
@@ -0,0 +1,7 @@
+// let openModel = document.querySelector('.modal-btn')
+// let closeModel = document.querySelector('.cancel')
+// let modal = document.querySelector('.two')
+
+// openModel.addEventListener('click' , function(){
+//     modal.classList.add('toggle')
+// })
