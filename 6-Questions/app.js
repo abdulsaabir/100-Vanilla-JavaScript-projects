7a7c409a509c29cc882c435eb048bd397679d7fb
eng-abdulsaabirhaaji <saabirhaaji10@gmail.com>
2022-09-06 18:16:45 +0300

    que

---
+++ 6-Questions/app.js
@@ -0,0 +1,9 @@
+//using selectors inside the element
+// traversing the dom
+
+let click = document.querySelectorAll('Button')
+
+let hiddenText1 = document.querySelector(".hiddentext1")
+let hiddenText2 = document.querySelector(".hiddentext2")
+let hiddenText3 = document.querySelector(".hiddentext3")
+
