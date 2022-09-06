841450a6a5aeb0137fac3413cfdba36854c5a0dd
eng-abdulsaabirhaaji <saabirhaaji10@gmail.com>
2022-09-05 19:04:38 -0700

    Responsive Navbar Project

--- nav-project/app.js
+++ nav-project/app.js
@@ -1,10 +1,11 @@
 let toggle = document.querySelector(".burger")
 let navSide = document.querySelector(".nav-links")
-let navLinks = document.querySelectorAll('.nav-links li')
+let socialMedia =  document.querySelector('.social-icons')
 
 
 
 toggle.addEventListener('click',() => {
     navSide.classList.toggle('nav-active')
     toggle.classList.toggle('rotate');
+    socialMedia.classList.toggle('social-icons-toggle')
 })
