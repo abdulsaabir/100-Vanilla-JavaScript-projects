a946dbf1c252fb5b66f8d21b60f320457ac0ce9a
eng-abdulsaabirhaaji <saabirhaaji10@gmail.com>
2022-09-07 09:39:56 +0300

    still

--- 6-Questions/app.js
+++ 6-Questions/app.js
@@ -2,3 +2,24 @@
 // traversing the dom
 
 
+let btns = document.querySelectorAll(".question-btn")
+let parent = document.querySelectorAll('.parent')
+
+btns.forEach(btn =>{
+    btn.addEventListener('click' , (e)=>{
+        let parentOfbtn  = e.currentTarget.parentElement.parentElement
+        let currenbtn = e.currentTarget
+        parentOfbtn.classList.toggle("showAll")
+        parent.forEach(close => {
+            let currenparent = close.classList
+           if(currenparent.contains("showAll"))
+           {
+            console.log(currenparent)
+           }
+           else{
+            currenparent.remove("showAll")
+           }
+        })
+
+    })
+})
