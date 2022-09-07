4504ac812eaec1c7ea908610eaa153675a91d4bc
eng-abdulsaabirhaaji <saabirhaaji10@gmail.com>
2022-09-07 08:28:12 +0300

    still

--- 6-Questions/app.js
+++ 6-Questions/app.js
@@ -1,12 +1,3 @@
 //using selectors inside the element
 // traversing the dom
- 
-let btns = document.querySelectorAll(".question-btn")
-let theParent = document.querySelector(".parent")
 
-btns.forEach(btn =>{
-    btn.addEventListener('click' , (e)=>{
-    let parent = e.currentTarget.parentElement.parentElement
-    parent.classList.toggle('showAll')
-    })
-})
