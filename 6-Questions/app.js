2c9b92337398092967287b0ad8cbe876d1367830
eng-abdulsaabirhaaji <saabirhaaji10@gmail.com>
2022-09-07 08:28:45 +0300

    still

--- 6-Questions/app.js
+++ 6-Questions/app.js
@@ -1,3 +1,22 @@
 //using selectors inside the element
 // traversing the dom
 
+
+let theParent = document.querySelectorAll(".parent")
+
+theParent.forEach(parentElement => {
+    let clickedbtn = parentElement.querySelector('.question-btn')
+    clickedbtn.addEventListener('click' , () =>{
+        parentElement.classList.toggle('showAll')
+    })
+})
+
+// let btns = document.querySelectorAll(".question-btn")
+// let theParent = document.querySelector(".parent")
+
+// btns.forEach(btn =>{
+//     btn.addEventListener('click' , (s)=>{
+//     let parent = s.currentTarget.parentElement.parentElement
+//     parent.classList.toggle('showAll')
+//     })
+// })
