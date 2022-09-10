8cfcdd8ad748e4a06f940dd1867bfa1b32cc02db
eng-abdulsaabirhaaji <saabirhaaji10@gmail.com>
2022-09-10 11:55:41 +0300

    still

--- 11-CountDown-Timer/app.js
+++ 11-CountDown-Timer/app.js
@@ -13,21 +13,20 @@ const months = [
   'December',
 ];
 const weekdays = [
-  'Saturday',
   'Sunday',
   'Monday',
   'Tuesday',
   'Wednesday',
   'Thursday',
   'Friday',
-  
+  'Saturday',
 ];
 const giveaway = document.querySelector('.giveaway');
 const deadline = document.querySelector('.deadline');
 const dead = document.querySelector('.april');
 const items = document.querySelectorAll('.deadline-format h4');
 // 24 April 2020, 8:00am
-let dates =new Date(2022,9,14,10,40,30)
+let dates =new Date(2022,09,14,10,40,30)
 let year= dates.getFullYear()
 let hours= dates.getHours()
 let minutes= dates.getMinutes()
