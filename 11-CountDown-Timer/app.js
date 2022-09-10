136ccc58c7c0ca1875e618da83bf816b548c2f06
eng-abdulsaabirhaaji <saabirhaaji10@gmail.com>
2022-09-10 17:18:25 +0300

    Count-Down Timer Project

--- 11-CountDown-Timer/app.js
+++ 11-CountDown-Timer/app.js
@@ -27,12 +27,52 @@ const dead = document.querySelector('.april');
 const items = document.querySelectorAll('.deadline-format h4');
 // 24 April 2020, 8:00am
 
-let dates =new Date(2022,09,14,10,40,30)
+let newdate = new Date()
+let tempYear = newdate.getFullYear()
+let tempMoth = newdate.getMonth()
+let tempDay = newdate.getDate()
+let dates = new Date(tempYear, tempMoth , tempDay + 9 , 40 , 74 , 59 , 1000)
 let year= dates.getFullYear()
 let hours= dates.getHours()
 let minutes= dates.getMinutes()
 let month =dates.getMonth()
-let day= dates.getDay()
-window.addEventListener("DOMContentLoaded" ,() => {
-  dead.textContent = weekdays[day]+" "+months[month]+" "+year+", "+hours+":"+minutes+"am"
+let days= dates.getDay()
+
+dead.textContent = weekdays[days]+" "+months[month]+" "+year+", "+hours+":"+minutes+"am"
+
+
+let futureDate = dates.getTime()
+function GetRemaingTime() {
+let today = new Date().getTime()
+let t = futureDate - today
+
+let oneDay = 24 * 60 * 60 * 1000
+let oneHour = 60 * 60 * 1000
+let oneMinute = 60 * 1000
+let oneSecond = 1000
+let dayRemaing = Math.floor(t/oneDay)
+let HoursRemaining = Math.floor((t % oneDay) / oneHour)
+let MinutesRemaing = Math.floor((t % oneHour) / oneMinute)
+let secondsRemainng = Math.floor((t % oneMinute)/ oneSecond)
+
+let Values = [dayRemaing , HoursRemaining , MinutesRemaing , secondsRemainng]
+function format(item) {
+  if(item < 10)
+  {
+    return item = `0${item}`
+  }
+  return item
+}
+
+items.forEach((item, i )=> {
+  item.innerHTML = format(Values[i])
 })
+if (t < 0)
+{
+  clearInterval(countdown)
+  deadline.innerHTML = `<h3> CONGRATULATIONS ABDULSAABIR THE WINNER OF OUR GIVE AWAY <h3>`
+}
+
+}
+let countdown = setInterval(GetRemaingTime, 1000)
+GetRemaingTime()
