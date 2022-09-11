1092140fae8e07fb46c3d30ec2883ab141ff19f2
eng-abdulsaabirhaaji <saabirhaaji10@gmail.com>
2022-09-11 08:25:01 +0300

    Still Working

--- 12-Lorem-Ipsmum/app.js
+++ 12-Lorem-Ipsmum/app.js
@@ -27,13 +27,33 @@ form.addEventListener('submit' ,(e) =>{
   }
   else
   {
-    let newText = text.slice(0,result)
-    temptext = newText.map(item => {
-      return `<p class="result"> ${item} </p>`
-    }).join('')
-    loremText.innerHTML = temptext
-    }
+    let tempText = text.slice(0, result);
+    tempText = tempText
+      .map(function (item) {
+        return `<p class="result">${item}</p>`;
+      })
+      .join("");
+      loremText.innerHTML = tempText;
+  }
+    
   })
 
 
+  let tempText = text.slice(0, value);
+  tempText = tempText
+    .map(function (item) {
+    
+    })
+    .join("");
+  result.innerHTML = tempText;
 
+
+
+
+  let newText = text.slice(0,result)
+  let temptext = newText
+    .map(item => {
+      return `<p class="result">${item}</p>`;
+    })
+    .join("")
+    loremText.innerHTML = temptext
