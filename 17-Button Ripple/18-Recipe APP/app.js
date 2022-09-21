b87a878d8c54102609908a07291b5f6e80efe6c8
eng-abdulsaabirhaaji <saabirhaaji10@gmail.com>
2022-09-21 22:37:38 +0300

    still working

---
+++ 17-Button Ripple/18-Recipe APP/app.js
@@ -0,0 +1,9 @@
+let searchinput = document.getElementById('searchin')
+let searchBox = document.querySelector('.searchBox')
+let logo = document.querySelector('.logo')
+
+searchBox.addEventListener('click' , (e) => {
+    e.preventDefault()
+    searchinput.classList.add('display')
+    logo.classList.add('hide')
+})
