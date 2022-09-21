875b7da4e3b5d50dcd86fedbf8feed76b0abf036
eng-abdulsaabirhaaji <saabirhaaji10@gmail.com>
2022-09-21 08:17:13 +0300

    Ripple Project

--- 17-Button Ripple/app.js
+++ 17-Button Ripple/app.js
@@ -1,8 +1,8 @@
 let btn = document.querySelector('.btn')
 btn.addEventListener('mouseover' , (e) => {
     
-   let x = (e.pageX - btn.offsetLeft)
-   let y = (e.pageY - btn.offsetTop)
+   let x = (e.pageX -  btn.offsetLeft)
+   let y = (e.pageY -  btn.offsetTop)
  
    btn.style.setProperty('--xPos', y + "px");
    btn.style.setProperty('--yPos', x + "px");
