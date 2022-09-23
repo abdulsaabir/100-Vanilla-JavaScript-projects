27cb787b7cf6d63d09dc8ae322e317dc88f60409
Eng-Abdulsaabir-Haaji <saabirhaaji10@gmail.com>
2022-09-23 14:01:53 +0300

    still working

--- 18-Recipe APP/app.js
+++ 18-Recipe APP/app.js
@@ -1,9 +1,9 @@
-let searchinput = document.getElementById('searchin')
-let searchBox = document.querySelector('.searchBox')
-let logo =document.querySelector('.logo')
+let searchinput = document.getElementById("searchin");
+let searchBox = document.querySelector(".searchBox");
+let logo = document.querySelector(".logo");
 
-searchBox.addEventListener('click' , (e) => {
-    e.preventDefault()
-    searchinput.classList.add('display')
-    logo.classList.add('hide')
-})
+searchBox.addEventListener("click", (e) => {
+  e.preventDefault();
+  logo.classList.add("hide");
+  searchinput.classList.add("showSearch");
+});
