af6ba6cd8204826bdb7ec682a9c4bc81942a1fe0
Eng-Abdulsaabir-Haaji <saabirhaaji10@gmail.com>
2022-09-24 08:17:19 +0300

    still working

--- 18-Recipe APP/app.js
+++ 18-Recipe APP/app.js
@@ -1,8 +1,32 @@
 let searchinput = document.getElementById("searchin");
 let searchBox = document.querySelector(".searchBox");
 let logo = document.querySelector(".logo");
+let mealid = document.getElementById("mealid");
+let foottitle = document.getElementById("foodtitle");
+let favicon = document.querySelector(".icon");
+let favfood = document.querySelector(".Favorites");
+(function () {
+  fetch("https:www.themealdb.com/api/json/v1/1/random.php")
+    .then((res) => res.json())
+    .then((data) => displayFoods(data));
+
+  const displayFoods = (foods) => {
+    foods.meals.forEach((meal) => {
+      mealid.src = `${meal.strMealThumb}`;
+      foottitle.textContent = meal.strMeal;
+      // console.log(meal);
+    });
+  };
+})();
 
 searchBox.addEventListener("click", (e) => {
   e.preventDefault();
   searchinput.classList.toggle("showSearch");
+});
+
+favicon.addEventListener("click", (e) => {
+  let text = e.target.nextSibling.ap;
+  console.log(text);
+  // let foodname = text.textContent;
+  // console.log(foodname);
 });
