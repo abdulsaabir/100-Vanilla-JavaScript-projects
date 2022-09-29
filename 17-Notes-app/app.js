8fd9c0757d9c05be8e515d126ba08c55cc3ecf47
Eng-Abdulsaabir-Haaji <saabirhaaji10@gmail.com>
2022-09-29 15:09:37 +0300

    done

--- 17-Notes-app/app.js
+++ 17-Notes-app/app.js
@@ -1,8 +1,15 @@
 // let addNote = document.getElementById("Note");
 let savebtn = document.querySelector(".save");
 let textarea = document.querySelector(".textarea");
-let Titletext = document.querySelector("Titletext");
+let Titletext = document.querySelector(".Titletext");
 // addNote.addEventListener("click", () => {
 
 // });
-savebtn.addEventListener("click", () => {});
+savebtn.addEventListener("click", () => {
+  if (textarea.value === "" || Titletext.value === "") {
+    alert("haha");
+  } else {
+    let noteTitle = Titletext.value;
+    let notebody = textarea.value;
+  }
+});
