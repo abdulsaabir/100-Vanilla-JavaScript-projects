a62e45a36b5548c8b992587c781742e8fbdb539a
Eng-Abdulsaabir-Haaji <saabirhaaji10@gmail.com>
2022-10-01 09:28:52 +0300

    done

--- 17-Notes-app/app.js
+++ 17-Notes-app/app.js
@@ -1,9 +1,33 @@
+weekdays = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
 let addBox = document.querySelector(".addBox"),
   boxnote = document.querySelector(".notepopup"),
   cancel = document.querySelector(".cl"),
   save = document.querySelector(".save"),
-  notetitle = document.querySelector(".tiile"),
-  notebody = document.querySelector(".body");
+  notetitle = document.querySelector(".title"),
+  notebody = document.querySelector(".body"),
+  container = document.querySelector(".container");
 
 addBox.addEventListener("click", () => {
   boxnote.classList.remove("display");
@@ -16,4 +40,35 @@ cancel.addEventListener("click", () => {
 save.addEventListener("click", () => {
   let valuetitle = notetitle.value,
     valuebody = notebody.value;
+  getdate();
+  addtothebody(valuetitle, valuebody, getdate());
 });
+
+function getdate() {
+  let today = weekdays[new Date().getDay()],
+    todaydate = new Date().getDate(),
+    month = months[new Date().getMonth()],
+    year = new Date().getFullYear();
+  let wholeDate = `${today} ${addzeroz(todaydate)}-${month}-${year}`;
+  return wholeDate;
+}
+
+function addzeroz(n) {
+  return ((parseInt, 10), n < 10 ? "0" : "") + n;
+}
+
+function addtothebody(title, body, date) {
+  container.innerHTML += `<div class="note">
+  <article>
+    <h1>${title}</h1>
+    <p>
+    ${body}
+    </p>
+  </article>
+  <section class="footer">
+    <footer class="date">${date}</footer>
+    <footer class="settings">
+      <iconify-icon icon="carbon:overflow-menu-horizontal"></iconify-icon>
+    </footer>
+  </section>`;
+}
