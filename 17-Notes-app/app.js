43b3248cca3a3e5f4a0cb8e791594bb4ff74a5db
Eng-Abdulsaabir-Haaji <saabirhaaji10@gmail.com>
2022-10-01 08:55:12 +0300

    done

--- 17-Notes-app/app.js
+++ 17-Notes-app/app.js
@@ -1,6 +1,7 @@
 let addBox = document.querySelector(".addBox");
 let boxnote = document.querySelector(".notepopup");
 let cancel = document.querySelector(".cl");
+let save = document.querySelector(".save");
 
 addBox.addEventListener("click", () => {
   boxnote.classList.remove("display");
@@ -9,3 +10,5 @@ addBox.addEventListener("click", () => {
 cancel.addEventListener("click", () => {
   boxnote.classList.add("display");
 });
+
+save.addEventListener("click", () => {});
