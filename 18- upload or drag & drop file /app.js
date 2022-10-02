d7919144c902ef25f572fb99f184588bfb6978fd
Eng-Abdulsaabir-Haaji <saabirhaaji10@gmail.com>
2022-10-02 16:17:18 +0300

    Done

--- 18- upload or drag & drop file /app.js
+++ 18- upload or drag & drop file /app.js
@@ -4,6 +4,7 @@ document.querySelectorAll(".drop-zone__input").forEach((inputElement) => {
   dropZoneElement.addEventListener("click", (e) => {
     inputElement.click();
   });
+
   inputElement.addEventListener("change", (e) => {
     if (inputElement.files.length) {
       updateThumbnail(dropZoneElement, inputElement.files[0]);
@@ -28,6 +29,7 @@ document.querySelectorAll(".drop-zone__input").forEach((inputElement) => {
       inputElement.files = e.dataTransfer.files;
       updateThumbnail(dropZoneElement, e.dataTransfer.files[0]);
     }
+
     dropZoneElement.classList.remove("drop-zone--over");
   });
 });
@@ -37,24 +39,21 @@ document.querySelectorAll(".drop-zone__input").forEach((inputElement) => {
  *
  * @param {HTMLElement} dropZoneElement
  * @param {File} file
- *
- *
- *
- *  */
- function updateThumbnail(dropZoneElement, file) {
+ */
+function updateThumbnail(dropZoneElement, file) {
   let thumbnailElement = dropZoneElement.querySelector(".drop-zone__thumb");
 
-   // First time - remove the prompt
-   if (dropZoneElement.querySelector(".drop-zone__prompt")) {
+  // First time - remove the prompt
+  if (dropZoneElement.querySelector(".drop-zone__prompt")) {
     dropZoneElement.querySelector(".drop-zone__prompt").remove();
   }
-    // First time - there is no thumbnail element, so lets create it
-    if (!thumbnailElement) {
-      thumbnailElement = document.createElement("div");
-      thumbnailElement.classList.add("drop-zone__thumb");
-      dropZoneElement.appendChild(thumbnailElement);
-    }
 
+  // First time - there is no thumbnail element, so lets create it
+  if (!thumbnailElement) {
+    thumbnailElement = document.createElement("div");
+    thumbnailElement.classList.add("drop-zone__thumb");
+    dropZoneElement.appendChild(thumbnailElement);
+  }
 
   thumbnailElement.dataset.label = file.name;
 
@@ -66,4 +65,7 @@ document.querySelectorAll(".drop-zone__input").forEach((inputElement) => {
     reader.onload = () => {
       thumbnailElement.style.backgroundImage = `url('${reader.result}')`;
     };
-
+  } else {
+    thumbnailElement.style.backgroundImage = null;
+  }
+}
