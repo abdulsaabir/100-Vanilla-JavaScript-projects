251f660e19fbd7fb684f53bf8b297adef008cddc
Eng-Abdulsaabir-Haaji <saabirhaaji10@gmail.com>
2022-10-02 16:13:13 +0300

    Done

--- 18- upload or drag & drop file /app.js
+++ 18- upload or drag & drop file /app.js
@@ -15,3 +15,9 @@ document.querySelectorAll(".drop-zone__input").forEach((inputElement) => {
     e.preventDefault();
     dropZoneElement.classList.add("drop-zone--over");
   });
+
+  ["dragleave", "dragend"].forEach((type) => {
+    dropZoneElement.addEventListener(type, (e) => {
+      dropZoneElement.classList.remove("drop-zone--over");
+    });
+  });
