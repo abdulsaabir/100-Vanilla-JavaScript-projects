cf1c4ace1211135be33f9fd8bfb9ed183c86c63f
Eng-Abdulsaabir-Haaji <saabirhaaji10@gmail.com>
2022-10-02 13:23:52 +0300

    Done

--- 18- upload or drag & drop file /app.js
+++ 18- upload or drag & drop file /app.js
@@ -1,6 +1,17 @@
 const containerDrag = document.querySelector(".container");
 containerDrag.addEventListener("dragover", (e) => {
   e.preventDefault();
-  // containerDrag.style.backgroundColor = "black";
-  console.log(containerDrag);
+  containerDrag.classList.add("background");
+});
+containerDrag.addEventListener("drop", (e) => {
+  console.log(e.dataTransfer);
+});
+[("dragend", "dragleave")].forEach((type) => {
+  containerDrag.addEventListener(type, (e) => {
+    containerDrag.classList.remove("background");
+  });
+});
+
+containerDrag.addEventListener("click", (e) => {
+  e.preventDefault();
 });
