18b17ca3827bc8f4057307eacede776e4c6701eb
Eng-Abdulsaabir-Haaji <saabirhaaji10@gmail.com>
2022-10-02 16:12:51 +0300

    Done

--- 18- upload or drag & drop file /app.js
+++ 18- upload or drag & drop file /app.js
@@ -1,7 +1,12 @@
 document.querySelectorAll(".drop-zone__input").forEach((inputElement) => {
   const dropZoneElement = inputElement.closest(".drop-zone");
 
-  
+
   dropZoneElement.addEventListener("click", (e) => {
     inputElement.click();
+  });
+  inputElement.addEventListener("change", (e) => {
+    if (inputElement.files.length) {
+      updateThumbnail(dropZoneElement, inputElement.files[0]);
+    }
   });
