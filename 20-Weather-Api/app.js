4118a86f08dc371c3b40d1b210a813194cb3efb5
Eng-Abdulsaabir-Haaji <saabirhaaji10@gmail.com>
2022-10-03 19:27:17 +0300

    quote generator

--- 20-Weather-Api/app.js
+++ 20-Weather-Api/app.js
@@ -7,6 +7,13 @@ window.addEventListener("load", () => {
       long = position.coords.longitude;
       lat = position.coords.latitude;
       let api = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=1250c30e7025057655ffaea3ff929e53`;
+      fetch(api)
+        .then((response) => response.json())
+        .then((data) => {
+          let temp = data.main.temp;
+          let celc = temp - 273.15 + "C";
+          console.log(celc.slice(0, 5) + "C");
+        });
     });
   }
 });
