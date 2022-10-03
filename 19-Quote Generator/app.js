ce4fe31443a3240ea9064e8c19ba6debc297650a
Eng-Abdulsaabir-Haaji <saabirhaaji10@gmail.com>
2022-10-03 14:50:00 +0300

    Done

--- 19-Quote Generator/app.js
+++ 19-Quote Generator/app.js
@@ -1,40 +1,27 @@
 let qouteContent = document.querySelector(".qouteContent");
 const btnRandom = document.querySelector("button");
+let qoute = document.querySelector(".qoute");
+let author = document.querySelector(".author");
+btnRandom.addEventListener("click", getRndomQuote);
 window.addEventListener("DOMContentLoaded", () => {
   generate();
 
   function generate() {
     fetch("https://api.quotable.io/random")
       .then((response) => response.json())
-      .then((quote) => {
-        qouteContent.innerHTML = `
-    <div class="qoutetext">
-    <iconify-icon class="left" icon="bxs:quote-left"></iconify-icon>
-    <p class="qoute">
-    ${quote.content}
-    </p>
-    <iconify-icon class="right" icon="bxs:quote-right"></iconify-icon>
-    </div>
-    <div class="quoteAuthor">
-    <span class="line">__</span>
-    <span class="author">${quote.author}</span>
-    </div>`;
-        let qoute = document.querySelector(".qoute");
-        let author = document.querySelector(".author");
-        btnRandom.addEventListener("click", getRndomQuote());
-
-        function getRndomQuote() {
-          btnRandom.classList.add("loading");
-          btnRandom.textContent = "Loading..";
-          fetch("https://api.quotable.io/random")
-            .then((response) => response.json())
-            .then((result) => {
-              qoute.textContent = result.content;
-              author.textContent = result.author;
-              btnRandom.textContent = "New Quote";
-              btnRandom.classList.remove("loading");
-            });
-        }
-      });
-  }
+      .then((quote) => { 
 });
+
+
+function getRndomQuote() {
+  btnRandom.classList.add("loading");
+  btnRandom.textContent = "Loading..";
+  fetch("https://api.quotable.io/random")
+    .then((response) => response.json())
+    .then((result) => {
+      qoute.textContent = result.content;
+      author.textContent = result.author;
+      btnRandom.textContent = "New Quote";
+      btnRandom.classList.remove("loading");
+    });
+}
