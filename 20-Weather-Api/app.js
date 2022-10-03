3813279de511cf05c7b20276eef6b48b69332f67
Eng-Abdulsaabir-Haaji <saabirhaaji10@gmail.com>
2022-10-03 18:53:10 +0300

    quote generator

--- 20-Weather-Api/app.js
+++ 20-Weather-Api/app.js
@@ -7,17 +7,6 @@ window.addEventListener("load", () => {
       long = position.coords.longitude;
       lat = position.coords.latitude;
       let api = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=1250c30e7025057655ffaea3ff929e53`;
-      fetch(api)
-        .then((response) => response.json())
-        .then((data) => {
-          console.log(data);
-        });
     });
   }
 });
-// fetch("https://api.quotable.io/random")
-// .then((response) => response.json())
-// .then((quote) => {
-//   qoute.textContent = quote.content;
-//   author.textContent = quote.author;
-// });
