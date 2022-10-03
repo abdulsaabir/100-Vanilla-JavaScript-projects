475eb5b40c67ffc8c6184e149b7ce3857b00e89b
Eng-Abdulsaabir-Haaji <saabirhaaji10@gmail.com>
2022-10-03 14:43:48 +0300

    Done

--- 19-Quote Generator/app.js
+++ 19-Quote Generator/app.js
@@ -1,34 +1,42 @@
 let qouteContent = document.querySelector(".qouteContent");
 const btnRandom = document.querySelector("button");
 window.addEventListener("DOMContentLoaded", () => {
-  qouteContent.innerHTML = `
+  generate();
+
+  function generate() {
+    fetch("https://api.quotable.io/random")
+      .then((response) => response.json())
+      .then((quote) => {
+        qouteContent.innerHTML = `
     <div class="qoutetext">
     <iconify-icon class="left" icon="bxs:quote-left"></iconify-icon>
     <p class="qoute">
-      Never give up because you never know if the next try is going to be
-      the one that works.
+    ${quote.content}
     </p>
     <iconify-icon class="right" icon="bxs:quote-right"></iconify-icon>
     </div>
     <div class="quoteAuthor">
     <span class="line">__</span>
-    <span class="author">Mary kay Ash </span>
+    <span class="author">${quote.author}</span>
     </div>`;
 
-  let qoute = document.querySelector(".qoute");
-  let author = document.querySelector(".author");
-  btnRandom.addEventListener("click", getRndomQuote);
-
-  function getRndomQuote() {
-    btnRandom.textContent = "loading..";
-    btnRandom.classList.add("loading");
-    fetch("https://api.quotable.io/random")
-      .then((response) => response.json())
-      .then((result) => {
-        qoute.textContent = result.content;
-        author.textContent = result.author;
-        btnRandom.textContent = "New Quote";
-        btnRandom.classList.remove("loading");
+        let qoute = document.querySelector(".qoute");
+        let author = document.querySelector(".author");
+        getRndomQuote(btnRandom);
+        btnRandom.addEventListener("click", getRndomQuote);
       });
   }
 });
+
+function getRndomQuote(btn, quote, author) {
+  btnRandom.classList.add("loading");
+  btnRandom.textContent = "Loading..";
+  fetch("https://api.quotable.io/random")
+    .then((response) => response.json())
+    .then((result) => {
+      qoute.textContent = result.content;
+      author.textContent = result.author;
+      btnRandom.textContent = "New Quote";
+      btnRandom.classList.remove("loading");
+    });
+}
