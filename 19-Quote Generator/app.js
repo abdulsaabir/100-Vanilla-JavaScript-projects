308c9ade7c8d69f6c77ef4218c939cdfe3b74daf
Eng-Abdulsaabir-Haaji <saabirhaaji10@gmail.com>
2022-10-03 14:45:47 +0300

    Done

--- 19-Quote Generator/app.js
+++ 19-Quote Generator/app.js
@@ -19,23 +19,35 @@ window.addEventListener("DOMContentLoaded", () => {
     <span class="line">__</span>
     <span class="author">${quote.author}</span>
     </div>`;
-
         let qoute = document.querySelector(".qoute");
         let author = document.querySelector(".author");
         btnRandom.addEventListener("click", getRndomQuote);
 
-        function getRndomQuote() {
-          btnRandom.classList.add("loading");
-          btnRandom.textContent = "Loading..";
-          fetch("https://api.quotable.io/random")
-            .then((response) => response.json())
-            .then((result) => {
-              qoute.textContent = result.content;
-              author.textContent = result.author;
-              btnRandom.textContent = "New Quote";
-              btnRandom.classList.remove("loading");
-            });
-        }
+        // function getRndomQuote() {
+        //   btnRandom.classList.add("loading");
+        //   btnRandom.textContent = "Loading..";
+        //   fetch("https://api.quotable.io/random")
+        //     .then((response) => response.json())
+        //     .then((result) => {
+        //       qoute.textContent = result.content;
+        //       author.textContent = result.author;
+        //       btnRandom.textContent = "New Quote";
+        //       btnRandom.classList.remove("loading");
+        //     });
+        // }
       });
   }
 });
+
+function getRndomQuote() {
+  btnRandom.classList.add("loading");
+  btnRandom.textContent = "Loading..";
+  fetch("https://api.quotable.io/random")
+    .then((response) => response.json())
+    .then((result) => {
+      qoute.textContent = result.content;
+      author.textContent = result.author;
+      btnRandom.textContent = "New Quote";
+      btnRandom.classList.remove("loading");
+    });
+}
