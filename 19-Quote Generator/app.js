22ea9a09d9b6622649d0974b76b0bbd11e556712
Eng-Abdulsaabir-Haaji <saabirhaaji10@gmail.com>
2022-10-03 15:12:15 +0300

    Done

--- 19-Quote Generator/app.js
+++ 19-Quote Generator/app.js
@@ -1,5 +1,6 @@
 let qouteContent = document.querySelector(".qouteContent");
 const btnRandom = document.querySelector("button");
+let loader = document.querySelector(".loader");
 let qoute = document.querySelector(".qoute");
 let author = document.querySelector(".author");
 btnRandom.addEventListener("click", getRndomQuote);
@@ -9,9 +10,12 @@ window.addEventListener("DOMContentLoaded", () => {
   function generate() {
     fetch("https://api.quotable.io/random")
       .then((response) => response.json())
-      .then((quote) => { 
+      .then((quote) => {
+        qoute.textContent = quote.content;
+        author.textContent = quote.author;
+      });
+  }
 });
-
 
 function getRndomQuote() {
   btnRandom.classList.add("loading");
