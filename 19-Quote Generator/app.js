8ca35dd47530b356e5722572016548ab5c8ba016
Eng-Abdulsaabir-Haaji <saabirhaaji10@gmail.com>
2022-10-03 13:37:49 +0300

    Done

--- 19-Quote Generator/app.js
+++ 19-Quote Generator/app.js
@@ -1,10 +1,11 @@
 const btnRandom = document.querySelector("button");
+let qoute = document.querySelector(".qoute");
 
 function getRndomQuote() {
   fetch("https://api.quotable.io/random")
     .then((response) => response.json())
     .then((result) => {
-      console.log(result);
+      qoute.textContent = result.content;
     });
   //   console.log("clicked");
 }
