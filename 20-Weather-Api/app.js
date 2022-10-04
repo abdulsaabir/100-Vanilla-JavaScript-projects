fa9a1316092e7ed268ed77789c79e055d9d8545e
Eng-Abdulsaabir-Haaji <saabirhaaji10@gmail.com>
2022-10-04 08:18:51 +0300

    Done

--- 20-Weather-Api/app.js
+++ 20-Weather-Api/app.js
@@ -2,12 +2,18 @@ let lat,
   long,
   api,
   apikey = "1250c30e7025057655ffaea3ff929e53";
+function showPosition(position) {
+  alert(`${position.coords.longitude} - ${position.coords.latitude}`);
+}
 window.addEventListener("load", () => {
+  navigator.geolocation.getCurrentPosition(this.showPosition);
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition((position) => {
       lat = position.coords.latitude;
       long = position.coords.longitude;
     });
     api = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=1250c30e7025057655ffaea3ff929e53`;
+
+    console.log(api);
   }
 });
