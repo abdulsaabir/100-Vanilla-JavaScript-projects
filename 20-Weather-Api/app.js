56f6a9d744b78d726c68300b435b0de56d4d3f6a
Eng-Abdulsaabir-Haaji <saabirhaaji10@gmail.com>
2022-10-04 08:11:47 +0300

    Done

--- 20-Weather-Api/app.js
+++ 20-Weather-Api/app.js
@@ -1,19 +1,13 @@
+let lat,
+  long,
+  api,
+  apikey = "1250c30e7025057655ffaea3ff929e53";
 window.addEventListener("load", () => {
-  let long;
-  let lat;
-  let apikeu = "1250c30e7025057655ffaea3ff929e53";
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition((position) => {
-      long = position.coords.longitude;
       lat = position.coords.latitude;
-      let api = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=1250c30e7025057655ffaea3ff929e53`;
-      fetch(api)
-        .then((response) => response.json())
-        .then((data) => {
-          let temp = data.main.temp;
-          let celc = temp - 273.15 + "C";
-          console.log(celc.slice(0, 5) + "C");
-        });
+      long = position.coords.longitude;
     });
+    api = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=1250c30e7025057655ffaea3ff929e53`;
   }
 });
