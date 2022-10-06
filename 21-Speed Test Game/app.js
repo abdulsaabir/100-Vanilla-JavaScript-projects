a15306a0c455ab7da03b9a6063515362999f8816
Eng-Abdulsaabir-Haaji <saabirhaaji10@gmail.com>
2022-10-06 16:11:03 +0300

    done

--- 21-Speed Test Game/app.js
+++ 21-Speed Test Game/app.js
@@ -137,3 +137,4 @@ window.addEventListener("DOMContentLoaded", () => {
     .then((response) => response.json())
     .then((response) => console.log(response))
     .catch((err) => console.error(err));
+});
