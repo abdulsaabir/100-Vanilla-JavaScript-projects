2f4fc7c99f813124182d8002b1391733a11200cc
Eng-Abdulsaabir-Haaji <saabirhaaji10@gmail.com>
2022-10-06 16:12:37 +0300

    done

--- 21-Speed Test Game/app.js
+++ 21-Speed Test Game/app.js
@@ -118,7 +118,7 @@ window.addEventListener("DOMContentLoaded", () => {
   const encodedParams = new URLSearchParams();
   encodedParams.append(
     "data",
-    "Paraphrasing API used to integrate content checking system with your website. You can built a realtime par1yeturn how much number of queries used. It provides human quality and plagiarism free content."
+    "Paraphrasing API used to integrate content checking system with your website. You can built a realtime paraphrasing tool, just like our tool works. So you can also rewrite articles fully with this API. Each 100 words checking consumes one query. If you are checking content using Paraphraser api our system will also return how much number of queries used. It provides human quality and plagiarism free content."
   );
   encodedParams.append("lang", "en");
   encodedParams.append("mode", "1");
@@ -135,6 +135,5 @@ window.addEventListener("DOMContentLoaded", () => {
 
   fetch("https://paraphraser-apis.p.rapidapi.com/paraphrasing-apis", options)
     .then((response) => response.json())
-    .then((response) => console.log(response))
-    .catch((err) => console.error(err));
+    .then((response) => console.log(response));
 });
