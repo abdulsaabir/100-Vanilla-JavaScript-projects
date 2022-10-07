f023c2cb70e211b78766ee65a0b6bfa3ef2239f5
Eng-Abdulsaabir-Haaji <saabirhaaji10@gmail.com>
2022-10-07 20:00:14 +0300

    working good

--- English Dictionary/app.js
+++ English Dictionary/app.js
@@ -1,10 +1,33 @@
 let Searchplace = document.getElementById("Search");
 let cancelbtn = document.querySelector(".cancel");
-
+let form = document.querySelector("form");
+let iconsearch = document.querySelector(".btnsrch");
 function displaycancel() {
   if (Searchplace.value != "") {
     cancelbtn.style.display = "block";
   }
+  if (Searchplace.value === "") {
+    cancelbtn.style.display = "none";
+  }
+}
+
+window.addEventListener("click", (e) => {
+  if (e.target.id == "Search") {
+    form.classList.add("color");
+    iconsearch.classList.add("colorbtn");
+  } else {
+    form.classList.remove("color");
+    iconsearch.classList.remove("colorbtn");
+  }
+});
+
+function inputKeyUp(e) {
+  e.preventDefault();
+  if (e.key === "Enter") {
+    // submit
+    console.log("neter");
+  }
 }
 
 Searchplace.addEventListener("input", displaycancel);
+Searchplace.addEventListener("", inputKeyUp);
