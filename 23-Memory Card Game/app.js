567d61fe415221a027e63cde5a778c0399bf1ae9
Eng-Abdulsaabir-Haaji <saabirhaaji10@gmail.com>
2022-10-11 11:09:54 +0300

    Working

--- 23-Memory Card Game/app.js
+++ 23-Memory Card Game/app.js
@@ -1,4 +1,18 @@
 let cards = document.querySelectorAll(".memory-card");
-function flipcards() {}
+function flipcards() {
+  this.classList.add("flip");
+}
 
+(function shuffle() {
+  cards.forEach((card) => {
+    let RandomNumber = Math.floor(Math.random * 12);
+    card.style.order = RandomNumber;
+  });
+})();
+(function shuffle() {
+  cards.forEach((card) => {
+    let randomPos = Math.floor(Math.random() * 12);
+    card.style.order = randomPos;
+  });
+})();
 cards.forEach((card) => card.addEventListener("click", flipcards));
