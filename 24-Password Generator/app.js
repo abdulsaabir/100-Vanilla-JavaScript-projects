9f7969864f7fb7e68969d2fe3afe594277720d64
Eng-Abdulsaabir-Haaji <saabirhaaji10@gmail.com>
2022-10-11 17:32:14 +0300

    js

--- 24-Password Generator/app.js
+++ 24-Password Generator/app.js
@@ -1,7 +1,10 @@
 let passworDdisplay = document.querySelector(".password");
 let passlength = document.getElementById("pass");
 let passlengthOutput = document.querySelector(".details span");
+let generatePassword = document.querySelector("button");
 
 passlength.onchange = function () {
   passlengthOutput.textContent = passlength.value;
 };
+
+generatePassword.addEventListener("click", generate);
