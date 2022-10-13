6bbad3db7c806c144744f276b3393f6846dfadc5
Eng-Abdulsaabir-Haaji <saabirhaaji10@gmail.com>
2022-10-13 11:30:46 +0300

    starting

---
+++ 26-Word Scramble/app.js
@@ -0,0 +1,23 @@
+let word = document.querySelector(".word");
+let hint = document.querySelector(".hint");
+let time = document.querySelector(".time");
+let Input = document.querySelector("input");
+let refresh = document.querySelector(".refresh");
+let check = document.querySelector(".check");
+
+function GenerateWord() {
+  fetch("https://random-word-api.herokuapp.com/word")
+    .then((resp) => resp.json())
+    .then((data) => {
+      hint.textContent = data[0];
+      let Word = data[0].toUpperCase().split("").sort();
+      //   time.textContent = word;
+      //   for (let i in Word) word.textContent += Word[getrandomNUmber(Word)];
+    });
+  // .then((words) => (word.textContent = word));
+}
+
+window.addEventListener("load", GenerateWord);
+let getrandomNUmber = (word) => {
+  return Math.floor(Math.random() * word.length);
+};
