9fc8e2c275ead8a4446a5cc3ce691d1ce99d27d5
Eng-Abdulsaabir-Haaji <saabirhaaji10@gmail.com>
2022-10-19 16:48:56 +0300

    done

---
+++ 28-Currency Convertor/dist/app.js
@@ -0,0 +1,4 @@
+let apikey = "DLs7YzWdqUfcmPIWeTLaRBLukHG1MOtI";
+fetch(`http://data.fixer.io/api/latest?access_key=${apikey}}&format=1`);
+
+http://data.fixer.io/api/latest?access_key=DLs7YzWdqUfcmPIWeTLaRBLukHG1MOtI&format=1
