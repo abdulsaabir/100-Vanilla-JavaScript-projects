f244e21d49cb9b9d96b2f05d59e75afbca8d66fb
Eng-Abdulsaabir-Haaji <saabirhaaji10@gmail.com>
2022-10-19 17:16:44 +0300

    done

---
+++ 28-Currency Convertor/app.js
@@ -0,0 +1,18 @@
+let amountMoney = document.querySelector(".amountMoney");
+let fromMoney = document.querySelector(".fromMoney");
+let toMoney = document.querySelector(".toMoney");
+
+const getRate = async (fromCurrency, toCUrrency) => {
+  let requestURL = `https://api.exchangerate.host/convert?from=${fromCurrency}&to=${toCUrrency}`;
+  let request = new XMLHttpRequest();
+  request.open("GET", requestURL);
+  request.responseType = "json";
+  request.send();
+  let response;
+  request.onload = function () {
+    let response = request.response;
+    console.log(response);
+  };
+};
+
+getRate("USD", "EURO");
