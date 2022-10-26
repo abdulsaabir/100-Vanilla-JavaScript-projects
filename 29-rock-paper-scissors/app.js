716ab49e11748bdcac5c9646c5a43cd97eefd444
Eng-Abdulsaabir-Haaji <saabirhaaji10@gmail.com>
2022-10-26 14:47:29 +0300

    done

---
+++ 29-rock-paper-scissors/app.js
@@ -0,0 +1,12 @@
+let Played = document.querySelectorAll("#play");
+let ComputerCHoice = ["rock", "paper", "Scissor"];
+let result = document.querySelector(".result");
+let user = document.querySelector(".user");
+let comp = document.querySelector(".comp");
+let userCounter = 0;
+let compCounter = 0;
+Played.forEach((item) => {
+  item.addEventListener("click", (e) => {
+    let currenTarget = e.currentTarget;
+  });
+});
