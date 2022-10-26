2c1733390ea00f15d54b4449457803e5f75fc5ee
Eng-Abdulsaabir-Haaji <saabirhaaji10@gmail.com>
2022-10-26 15:12:43 +0300

    DONE

--- 29-rock-paper-scissors/app.js
+++ 29-rock-paper-scissors/app.js
@@ -1,4 +1,4 @@
-let Played = document.querySelectorAll("#play");
+let Played = document.querySelectorAll(".play");
 let ComputerCHoice = ["rock", "paper", "Scissor"];
 let result = document.querySelector(".result");
 let user = document.querySelector(".user");
@@ -7,6 +7,70 @@ let userCounter = 0;
 let compCounter = 0;
 Played.forEach((item) => {
   item.addEventListener("click", (e) => {
-    let currenTarget = e.currentTarget;
+    let currenTarget = e.currentTarget.dataset.id;
+    let CompCHoice = GenreteComputer();
+    comPareGame(currenTarget, CompCHoice);
   });
 });
+
+const GenreteComputer = () => {
+  let RandomNumber = Math.floor(Math.random() * 3);
+  return ComputerCHoice[RandomNumber];
+};
+
+const comPareGame = (player, comp) => {
+  if (player === comp) {
+    result.textContent = "it's tie game";
+    return;
+  }
+  if (player === "rock") {
+    if (comp === "Scissor") {
+      result.textContent = "player Wins";
+      userCounter++;
+    } else {
+      result.textContent = "Computer Wins";
+      compCounter++;
+    }
+  }
+
+  if (player === "paper") {
+    if (comp === "Scissor") {
+      result.textContent = "Computer Wins";
+      compCounter++;
+    } else {
+      result.textContent = "Player Wins";
+      userCounter++;
+    }
+  }
+
+  if (player === "Scissor") {
+    if (comp === "rock") {
+      result.textContent = "computer Wins";
+      compCounter++;
+    } else {
+      result.textContent = "Player Wins";
+      userCounter++;
+    }
+  }
+  updateTHescore();
+  gameOver();
+};
+
+function updateTHescore() {
+  user.textContent = userCounter;
+  comp.textContent = compCounter;
+}
+
+function gameOver() {
+  if (userCounter === 10 || compCounter === 10) {
+    result.textContent =
+      userCounter > compCounter
+        ? "Game over and User Won"
+        : "Game over and Computer Won";
+
+    userCounter = 0;
+    compCounter = 0;
+    user.textContent = userCounter;
+    comp.textContent = compCounter;
+  }
+}
