35dae3a00281272bfe6de2ebe781f22c6e23c835
Eng-Abdulsaabir-Haaji <saabirhaaji10@gmail.com>
2022-10-26 15:13:36 +0300

    done

--- 29-rock-paper-scissors/app.js
+++ 29-rock-paper-scissors/app.js
@@ -16,7 +16,7 @@ Played.forEach((item) => {
 const GenreteComputer = () => {
   let RandomNumber = Math.floor(Math.random() * 3);
   return ComputerCHoice[RandomNumber];
-// };
+};
 
 // const comPareGame = (player, comp) => {
 //   if (player === comp) {
