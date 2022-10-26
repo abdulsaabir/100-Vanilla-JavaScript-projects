44641e65337080380eb58715df4001f6ab1e6356
Eng-Abdulsaabir-Haaji <saabirhaaji10@gmail.com>
2022-10-26 15:13:18 +0300

    done

--- 29-rock-paper-scissors/app.js
+++ 29-rock-paper-scissors/app.js
@@ -13,64 +13,64 @@ Played.forEach((item) => {
   });
 });
 
-const GenreteComputer = () => {
-  let RandomNumber = Math.floor(Math.random() * 3);
-  return ComputerCHoice[RandomNumber];
-};
+// const GenreteComputer = () => {
+//   let RandomNumber = Math.floor(Math.random() * 3);
+//   return ComputerCHoice[RandomNumber];
+// };
 
-const comPareGame = (player, comp) => {
-  if (player === comp) {
-    result.textContent = "it's tie game";
-    return;
-  }
-  if (player === "rock") {
-    if (comp === "Scissor") {
-      result.textContent = "player Wins";
-      userCounter++;
-    } else {
-      result.textContent = "Computer Wins";
-      compCounter++;
-    }
-  }
+// const comPareGame = (player, comp) => {
+//   if (player === comp) {
+//     result.textContent = "it's tie game";
+//     return;
+//   }
+//   if (player === "rock") {
+//     if (comp === "Scissor") {
+//       result.textContent = "player Wins";
+//       userCounter++;
+//     } else {
+//       result.textContent = "Computer Wins";
+//       compCounter++;
+//     }
+//   }
 
-  if (player === "paper") {
-    if (comp === "Scissor") {
-      result.textContent = "Computer Wins";
-      compCounter++;
-    } else {
-      result.textContent = "Player Wins";
-      userCounter++;
-    }
-  }
+//   if (player === "paper") {
+//     if (comp === "Scissor") {
+//       result.textContent = "Computer Wins";
+//       compCounter++;
+//     } else {
+//       result.textContent = "Player Wins";
+//       userCounter++;
+//     }
+//   }
 
-  if (player === "Scissor") {
-    if (comp === "rock") {
-      result.textContent = "computer Wins";
-      compCounter++;
-    } else {
-      result.textContent = "Player Wins";
-      userCounter++;
-    }
-  }
-  updateTHescore();
-  gameOver();
-};
+//   if (player === "Scissor") {
+//     if (comp === "rock") {
+//       result.textContent = "computer Wins";
+//       compCounter++;
+//     } else {
+//       result.textContent = "Player Wins";
+//       userCounter++;
+//     }
+//   }
+//   updateTHescore();
+//   gameOver();
+// };
 
-function updateTHescore() {
-  user.textContent = userCounter;
-  comp.textContent = compCounter;
-}
+// function updateTHescore() {
+//   user.textContent = userCounter;
+//   comp.textContent = compCounter;
+// }
 
-function gameOver() {
-  if (userCounter === 10 || compCounter === 10) {
-    result.textContent =
-      userCounter > compCounter
-        ? "Game over and User Won"
-        : "Game over and Computer Won";
+// function gameOver() {
+//   if (userCounter === 10 || compCounter === 10) {
+//     result.textContent =
+//       userCounter > compCounter
+//         ? "Game over and User Won"
+//         : "Game over and Computer Won";
 
-    userCounter = 0;
-    compCounter = 0;
-    user.textContent = userCounter;
-    comp.textContent = compCounter;
-  }
-}
+//     userCounter = 0;
+//     compCounter = 0;
+//     user.textContent = userCounter;
+//     comp.textContent = compCounter;
+//   }
+// }
