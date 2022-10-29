60332487132385e6cd80cbf405164d57d137e0ac
Eng-Abdulsaabir-Haaji <saabirhaaji10@gmail.com>
2022-10-29 11:07:13 +0300

    Rock Paper Scissors

--- 30-Whack-Male-game/app.js
+++ 30-Whack-Male-game/app.js
@@ -1,3 +1,13 @@
+let HoLES = document.querySelectorAll(".hole");
+let LastHole;
+
 function getRandomTime(min, max) {
-  return Math.floor(Math.random * (max - min + min));
+  return Math.floor(Math.random() * (max - min + min));
+}
+
+function getRandomHoles() {
+  let RandomHOle = Math.floor(Math.random() * HoLES.length);
+
+  let Hole = HoLES[RandomHOle];
+  return Hole;
 }
