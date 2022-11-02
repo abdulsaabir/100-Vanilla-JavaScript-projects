0725d142732bae5bc4ddac476b93c30a92875bf7
Abdulsaabir <saabirhaaji10@gmail.com>
2022-11-02 21:05:53 +0300

    Calculator

--- 31-Calculator/app.js
+++ 31-Calculator/app.js
@@ -1,19 +1,35 @@
 const numberBtn = document.querySelectorAll('[data-number]')
 const operationBtn = document.querySelectorAll('[data-operation]')
 let currentoperand = document.querySelector('.current-operand')
-
+let previousoperand = document.querySelector('.previous-operand')
+let operand;
 let numbernow = "";
 numberBtn.forEach(number => {
    number.addEventListener('click' , e => {
    numbernow=  numbernow.concat(e.target.textContent)
    currentoperand.textContent = numbernow
 
-
    })
 })
 
 operationBtn.forEach(oper => {
     oper.addEventListener('click' , e=>{
-     console.log(e.target.textContent)   
+        // if(currentoperand.textContent) return
+        if(operand === "÷") operand = "/"
+    //  console.log(operand)
+    // console.log(Number(operand))
+     if(previousoperand.textContent && currentoperand.textContent)
+     {
+      previousoperand.textContent =   (eval(`${previousoperand.textContent} ${operand} ${currentoperand.textContent}`))
+      currentoperand.textContent = ""
+      numbernow = ""
+     operand = e.target.textContent
+      return
+     }
+     previousoperand.textContent = currentoperand.textContent
+     currentoperand.textContent = ""
+     numbernow = ""
+    operand = e.target.textContent
+  
     })
 })
