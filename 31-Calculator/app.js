d5e96c7249b77a33d2b69e338a47d48fec91c384
Abdulsaabir <saabirhaaji10@gmail.com>
2022-11-03 13:17:03 +0300

    Calculator

--- 31-Calculator/app.js
+++ 31-Calculator/app.js
@@ -2,8 +2,15 @@ const numberBtn = document.querySelectorAll('[data-number]')
 const operationBtn = document.querySelectorAll('[data-operation]')
 let currentoperand = document.querySelector('.current-operand')
 let previousoperand = document.querySelector('.previous-operand')
-let operand;
-let numbernow = "";
 let  dataquals = document.querySelector('[ data-equals]')
 let dataallclear = document.querySelector('[data-all-clear]')
 let datadelete = document.querySelector('[data-delete]')
+
+
+class Calculator {
+    constructor(previousoperand , currentoperand)
+    {
+        this.previousoperand = previousoperand;
+        this.currentoperand = currentoperand
+    }
+}
