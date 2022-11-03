cfc85473314b04e01f21ee46979d6ca797c4b4fa
Abdulsaabir <saabirhaaji10@gmail.com>
2022-11-03 13:27:59 +0300

    Calculator

--- 31-Calculator/app.js
+++ 31-Calculator/app.js
@@ -1,3 +1,36 @@
+class CalculatorClass {
+    constructor(previousoperand , currentoperand)
+    {
+        this.previousoperand = previousoperand;
+        this.currentoperand = currentoperand
+        this.clear()
+    }
+
+    clear(){
+        this.currentoperand = ""
+        this.previousoperand=""
+        this.operand=""
+        this.operation = ""
+    }
+    displayoperand(operand){
+        this.operand = operand
+    }
+    updateCalc(){
+        this.currentoperand.text = this.operand
+    }
+    calCulate(){
+        console.log(this.currentoperand)
+    }
+    delete(){
+
+    }
+    equal(){
+
+    }
+}
+
+
+
 const numberBtn = document.querySelectorAll('[data-number]')
 const operationBtn = document.querySelectorAll('[data-operation]')
 let currentoperand = document.querySelector('.current-operand')
@@ -6,11 +39,14 @@ let  dataquals = document.querySelector('[ data-equals]')
 let dataallclear = document.querySelector('[data-all-clear]')
 let datadelete = document.querySelector('[data-delete]')
 
+let Calculator = new CalculatorClass()
 
-class Calculator {
-    constructor(previousoperand , currentoperand)
-    {
-        this.previousoperand = previousoperand;
-        this.currentoperand = currentoperand
-    }
-}
+
+numberBtn.forEach(btn =>{
+    btn.addEventListener('click' , e => {
+        Calculator.displayoperand(e.target.innerText)
+        Calculator.calCulate()
+    })
+})
+
+
